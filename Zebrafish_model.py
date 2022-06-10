bd83374a4991bb97d062a33cd863d6cb380f55f4
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-06-10 20:33:57 +0200

    fix in model imports

--- Zebrafish_model.py
+++ Zebrafish_model.py
@@ -1,7 +1,8 @@
-from kiwisolver import Expression
 import snakes.plugins
-snakes.plugins.load('gv', 'snakes.nets', 'nets')
-from nets import *
+snakes.plugins.load('gv', 'snakes.nets', 'my_nets')
+from snakes.nets import *
+from my_nets import *  # required to draw networks
+
 
 class ZebraMol():
     def __init__(self, kappas=None, t=0, save_net_img=None):
@@ -18,64 +19,63 @@ class ZebraMol():
                             'k_G,e': 0.00743,
                             'k_S,e': 0.000664, }
         else: self.kappas = kappas
-        self.create_model()
-
+        self.net = self.create_model()
 
         if save_net_img is not None:
             self.net.draw(save_net_img, engine='dot')
 
-
     def create_model(self):
-        self.net = PetriNet('Zebrafish Paracetamol net')
+        net = PetriNet('Zebrafish Paracetamol net')
         
-        self.net.add_place(Place('input')) # paracetamol in water
-        self.net.add_place(Place('P')) # paracetamol in zebrafish
-        add_sequence(net=self.net,
+        net.add_place(Place('input')) # paracetamol in water
+        net.add_place(Place('P')) # paracetamol in zebrafish
+        add_sequence(net=net,
                     name="water to zebrafish absorption rate",
                     from_place='input',
                     to_place='P',
                     in_var="x",
-                    expr=f"x *{self.kappas['k_a']} ")
+                    expr=f"x * {self.kappas['k_a']} ")
 
-        self.net.add_place(Place('P_excreted'))
-        add_sequence(net=self.net,
+        net.add_place(Place('P_excreted'))
+        add_sequence(net=net,
                     name="paracetamol excretion",
                     from_place='P',
                     to_place='P_excreted',
                     in_var="x",
-                    expr=f"x *{self.kappas['k_P,e']} ")
+                    expr=f"x * {self.kappas['k_P,e']} ")
     
-        self.net.add_place(Place('G'))
-        add_sequence(net=self.net,
+        net.add_place(Place('G'))
+        add_sequence(net=net,
                     name="Glucuronidation creation",
                     from_place='P',
                     to_place='G',
                     in_var="x",
-                    expr=f"x *{self.kappas['k_PG,f']} ")
+                    expr=f"x * {self.kappas['k_PG,f']} ")
         
-        self.net.add_place(Place('G_excreted'))
-        add_sequence(net=self.net,
+        net.add_place(Place('G_excreted'))
+        add_sequence(net=net,
                     name="paracetamol-glucuronide excretion",
                     from_place='G',
                     to_place='G_excreted',
                     in_var="x",
                     expr=f"x *{self.kappas['k_G,e']} ")
         
-        self.net.add_place(Place('S'))
-        add_sequence(net=self.net,
+        net.add_place(Place('S'))
+        add_sequence(net=net,
                     name="Sulfation creation",
                     from_place='P',
                     to_place='S',
                     in_var="x",
-                    expr=f"x *{self.kappas['k_PS,f']} ")
+                    expr=f"x * {self.kappas['k_PS,f']} ")
 
-        self.net.add_place(Place('S_out'))
-        add_sequence(net=self.net,
+        net.add_place(Place('S_out'))
+        add_sequence(net=net,
                     name="paracetamol-sulfate excretion",
                     from_place='S',
                     to_place='S_out',
                     in_var="x",
-                    expr=f"x *{self.kappas['k_S,e']} ")
+                    expr=f"x * {self.kappas['k_S,e']} ")
+        return net
 
 def add_sequence(net, name, from_place, to_place, in_var, expr, t_exp=None):
     """ Params:
